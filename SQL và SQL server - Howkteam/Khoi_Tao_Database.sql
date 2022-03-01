5a4453243b682f9227ec5fc25c0c02acc865996f
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2022-03-01 15:49:31 +0700

    Add files via upload

---
+++ SQL và SQL server - Howkteam/Khoi_Tao_Database.sql
@@ -0,0 +1,15 @@
+
+ -- tao DB1
+ -- day la cach tao comment trong SQL (su dung --)
+CREATE DATABASE SQLDBQuery
+
+-- moi ten DB la duy nhat
+
+-- De thuc thi lenh co 2 cach
+-- -- nhan F5 se thuc thi lenh
+-- -- nhan Execute
+
+-- Neu boi den dong lenh nao thi se thuc thi dong do
+
+-- Neu khong boi den thi thuc thi ca file query
+
