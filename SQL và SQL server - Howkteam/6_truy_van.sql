46557e64b095ff9b6931106945b176ee2bd74479
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2022-03-01 22:51:47 +0700

    Add files via upload

---
+++ SQL và SQL server - Howkteam/6_truy_van.sql
@@ -0,0 +1,32 @@
+﻿
+USE HowKteam
+GO
+
+-- cấu trúc truy vấn:
+
+SELECT * FROM dbo.BOMON -- lấy hết tất cả dữ liệu trong bảng bộ môn
+
+-- lấy mã bộ môn + tên bộ môn trong bảng bộ môn
+SELECT MaBM, TenBM FROM dbo.BOMON
+
+-- đổi tên cột hiển thị
+SELECT MaBM AS '1', TenBM AS '2' FROM dbo.BOMON
+
+-- xuất ra mã giáo viên + tên + tên bộ môn giáo viên đó dạy
+SELECT MAGV, HOTEN, TENBM FROM dbo.GIAOVIEN, dbo.BOMON
+
+
+
+-- bài tập
+
+-- 1. Truy xuất thông tin của bảng tham gia đề tài
+SELECT * FROM dbo.THAMGIADT
+
+-- 2. Lấy ra Mã khoa và tên khoa tương ứng
+SELECT MAKHOA, TENKHOA FROM dbo.KHOA
+
+-- 3. Lấy ra họ Mã GV, tên GV và họ tên người thân tương ứng
+SELECT GV.MAGV, GV.HOTEN, NT.TEN FROM GIAOVIEN AS GV, dbo.NGUOITHAN AS NT
+
+-- 4. Lấy ra Mã GV, tên GV và tên khoa của giáo viên đó làm việc. Gợi ý: bộ môn nằm trong khoa
+SELECT GV.MAGV, GV.HOTEN, K.TENKHOA FROM GIAOVIEN AS GV, KHOA AS K
---
+++ SQL và SQL server - Howkteam/7_truy_van_voi_dieu_kien.sql
@@ -0,0 +1,53 @@
+﻿USE HowKteam
+GO
+
+
+SELECT GV.MAGV, HOTEN, NT.TEN FROM dbo.GIAOVIEN AS GV, dbo.NGUOITHAN AS NT
+WHERE GV.MAGV = NT.MAGV
+
+-- lấy ra giáo viên có lương > 2000
+SELECT * FROM dbo.GIAOVIEN
+WHERE LUONG > 2000
+
+
+-- lấy ra giáo viên là nữ và lương > 2000
+SELECT * FROM dbo.GIAOVIEN
+WHERE LUONG > 2000 AND PHAI = N'Nữ'
+
+-- lấy ra giáo viên lớn hơn 40 tuổi
+	-- YEAR -> lấy ra năm của ngày
+	-- GETDATE --> lấy ra ngày hiện tại
+
+SELECT * FROM dbo.GIAOVIEN
+WHERE YEAR(GETDATE()) - YEAR(NGSINH) > 40
+
+-- lấy ra Họ Tên giáo viên, năm sinh và tuổi của giáo viên nhỏ hơn 40 tuổi
+SELECT HOTEN, NGSINH, GETDATE() - YEAR(NGSINH) FROM dbo.GIAOVIEN
+WHERE YEAR(GETDATE()) - YEAR(NGSINH) <= 40
+
+-- lấy ra mã GV, Tên GV và tên Khoa của giáo viên đó làm việc.
+SELECT gv.MAGV, gv.HOTEN, k.TENKHOA FROM dbo.GIAOVIEN AS gv, dbo.BOMON AS BM, dbo.KHOA AS K
+WHERE gv.MABM = BM.MABM AND BM.MAKHOA = K.MAKHOA
+
+-- lấy ra giáo viên là trưởng bộ môn
+SELECT GV.* FROM dbo.GIAOVIEN AS GV, dbo.BOMON AS BM
+WHERE GV.MAGV = BM.TRUONGBM
+
+-- đếm số lượng giáo viên 
+-- COUNT(*) --> đếm số lượng của tất cả record
+-- COUNT(tên field) --> đếm số lượng của field đó
+SELECT COUNT(*) AS N'Số lượng giáo viên' FROM dbo.GIAOVIEN
+
+-- đếm số lượng người thân của giáo viên có mã GV là 007
+-- COUNT(tên field) --> đếm số lượng của field đó
+SELECT COUNT(*) AS N'Số lượng người thân của giáo viên có mã 007' FROM dbo.GIAOVIEN, dbo.NGUOITHAN
+WHERE GIAOVIEN.MAGV = '007' AND GIAOVIEN.MAGV = NGUOITHAN.MAGV
+
+-- lấy ra tên giáo viên và tên đề tài người đó tham gia
+SELECT * FROM dbo.GIAOVIEN, dbo.THAMGIADT, dbo.DETAI
+WHERE GIAOVIEN.MAGV = THAMGIADT.MAGV AND THAMGIADT.MADT = DETAI.MADT
+
+-- lấy ra tên giáo viên và tên đề tài người đó tham gia khi người đó tham gia nhiều hơn 1 lần
+-- truy vấn lồng
+SELECT * FROM dbo.GIAOVIEN, dbo.THAMGIADT, dbo.DETAI
+WHERE GIAOVIEN.MAGV = THAMGIADT.MAGV AND THAMGIADT.MADT = DETAI.MADT
