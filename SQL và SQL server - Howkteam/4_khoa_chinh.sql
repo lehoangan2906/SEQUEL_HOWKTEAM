0107170bf1add19a20c0c72ab301deb358845d86
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2022-03-01 15:48:56 +0700

    Add files via upload

---
+++ SQL và SQL server - Howkteam/4_khoa_chinh.sql
@@ -0,0 +1,87 @@
+﻿CREATE DATABASE testPrimaryKey
+GO
+
+USE testPrimaryKey
+go
+
+-- unique: duy nhất trong toàn bộ bảng. Trường nào có unique thì không thể có 2 giá trị trùng nhau
+-- not null: trường đó không được phép null
+-- default: giá trị mặc định của trường đó nếu không gán giá trị khi insert
+CREATE TABLE testPrimaryKey1(
+	ID INT UNIQUE NOT NULL,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+)
+GO
+
+-- khi đã tạo bảng, muốn sửa cột thành primary key
+ALTER TABLE dbo.testPrimaryKey1 ADD PRIMARY KEY (ID)
+
+-- tạo primary key ngay khi tạo bảng
+CREATE TABLE testPrimaryKey2(
+	ID INT PRIMARY KEY,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+)
+GO
+
+-- tạo primary key gián tiếp trong khi tạo bảng
+CREATE TABLE testPrimaryKey3(
+	ID INT NOT NULL,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+
+	PRIMARY KEY(ID)
+)
+GO
+
+--  tạo primary key ngay trong bảng mà đặt tên cho key đó luôn
+CREATE TABLE testPrimaryKey4(
+	ID INT NOT NULL,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+
+	CONSTRAINT pktest4 -- sau này xóa key cho dễ
+	PRIMARY KEY (ID)
+)
+GO
+
+-- tạo primary key sau khi tạo bảng và đặt tên cho key đó 
+CREATE TABLE testPrimaryKey5(
+	ID INT NOT NULL,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+)
+GO
+
+ALTER TABLE dbo.testPrimaryKey5 ADD CONSTRAINT pktest5 PRIMARY KEY (ID)
+
+INSERT INTO dbo.testPrimaryKey1( ID)
+VALUES
+(   0  -- ID - int
+    )
+GO
+
+INSERT INTO dbo.testPrimaryKey1( ID)
+VALUES
+(   1  -- ID - int
+    )
+GO
+
+INSERT INTO dbo.testPrimaryKey1( ID)
+VALUES
+(   2  -- ID - int
+    )
+GO
+
+INSERT INTO dbo.testPrimaryKey1( ID)
+VALUES
+(   3  -- ID - int
+    )
+GO 
+
+-- khóa chính có 2 trường
+CREATE TABLE testPrimaryKey6(
+	ID1 INT NOT NULL,
+	ID2 INT NOT NULL,
+	NAME NVARCHAR(100) DEFAULT	N'Howkteam.com'
+
+	PRIMARY KEY (ID1, ID2)
+)
+GO
+
---
+++ SQL và SQL server - Howkteam/5_Foreign_Key.sql
@@ -0,0 +1,104 @@
+﻿CREATE DATABASE primary_Foreign
+GO
+
+USE primary_Foreign
+GO
+
+CREATE TABLE BoMon(
+	MaBM CHAR(10) PRIMARY KEY,
+	name NVARCHAR(100) default N'ten bo mon'
+)
+GO
+
+CREATE TABLE lop(
+	MaLop CHAR(10) NOT NULL,
+	Name NVARCHAR(100) DEFAULT N'ten lop'
+
+	PRIMARY KEY(MaLop)
+)
+GO
+
+-- điều kiện để tạo khóa ngoại
+	-- tham chiếu tới khóa chính
+	-- unique, not null
+	-- cùng kiểu dữ liệu
+	-- cùng số lượng trường có sắp xếp
+
+-- lợi ích:
+	-- đảm bảo toàn vẹn dữ liệu, không có trường hợp tham chiếu tới dữ liệu không tồn tại
+
+CREATE TABLE GiaoVien1(
+	MaGV CHAR(10) NOT NULL,
+	Name NVARCHAR(100) DEFAULT N'ten giao vien',
+	DiaChi NVARCHAR(100) DEFAULT N'dia chi giao vien',
+	NgaySinh DATE,
+	Sex BIT,
+    MaBM CHAR (10)
+
+	-- Tạo khóa ngoại ngay khi tạo bảng
+	FOREIGN KEY (MaBM) REFERENCES dbo.BoMon(MaBM)
+)
+GO
+
+CREATE TABLE GiaoVien(
+	MaGV CHAR(10) NOT NULL,
+	Name NVARCHAR(100) DEFAULT N'ten giao vien',
+	DiaChi NVARCHAR(100) DEFAULT N'dia chi giao vien',
+	NgaySinh DATE,
+	Sex BIT,
+    MaBM CHAR (10)
+)
+GO
+
+ALTER TABLE dbo.GiaoVien ADD PRIMARY KEY (MaGV)
+GO
+
+-- tạo khóa ngoại sau khi tạo bảng
+ALTER TABLE dbo.GiaoVien ADD FOREIGN KEY (MaBM) REFERENCES dbo.BoMon(MaBM)
+GO
+
+CREATE TABLE HocSinh(
+	MaHS CHAR(10) PRIMARY KEY,
+	Name NVARCHAR(100),
+	MaLop CHAR(10)
+)
+GO
+
+-- Tạo khóa ngoại sau khi tạo bảng
+ALTER TABLE dbo.HocSinh ADD FOREIGN KEY (MaLop) REFERENCES dbo.lop (MaLop)
+
+INSERT INTO dbo.BoMon(MaBM, name)
+VALUES
+(   'BM01', -- MaBM - char(10)
+    N'bộ môn 1' -- name - nvarchar(100)
+    )
+
+INSERT INTO dbo.BoMon(MaBM, name)
+VALUES
+(   'BM02', -- MaBM - char(10)
+    N'bộ môn 2' -- name - nvarchar(100)
+    )
+
+INSERT INTO dbo.BoMon(MaBM, name)
+VALUES
+(   'BM03', -- MaBM - char(10)
+    N'bộ môn 3' -- name - nvarchar(100)
+    )
+
+INSERT INTO dbo.GiaoVien
+(
+    MaGV,
+    Name,
+    DiaChi,
+    NgaySinh,
+    Sex,
+    MaBM
+)
+VALUES
+(   'GV01',        -- MaGV - char(10)
+    N'GV 1',       -- Name - nvarchar(100)
+    N'DC 1',       -- DiaChi - nvarchar(100)
+    GETDATE(), -- NgaySinh - date
+    1,      -- Sex - bit
+    'BM03'         -- MaBM - char(10)
+    )
